65666438406d110a798ce9a11b77ae53994e54b0
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-07-08 20:21:50 -0300

    tests: deletando user depois de concluir test

--- src/tests/user-controller.test.ts
+++ src/tests/user-controller.test.ts
@@ -1,7 +1,17 @@
 import { app } from '@/app';
+import { prisma } from '@/database/prisma';
 import request from 'supertest';
 
 describe('UsersController', () => {
+  let user_id: string;
+
+  // deleta o user depois do teste
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: { id: user_id },
+    });
+  });
+
   it('should create a new user successfully', async () => {
     const response = await request(app).post('/users').send({
       name: 'Test User',
@@ -12,5 +22,7 @@ describe('UsersController', () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
     expect(response.body.name).toBe('Test User');
+
+    user_id = response.body.id;
   });
 });
