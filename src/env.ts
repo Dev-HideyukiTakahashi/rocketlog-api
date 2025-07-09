50feebb3eb8a1a9e32b2602408405a8b1be326a9
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-07-08 21:37:57 -0300

    chore: add port .env

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import z from 'zod';
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
+  // coerce converte para number para garantir, caso venha string
+  PORT: z.coerce.number().default(3333),
 });
 
 export const env = envSchema.parse(process.env);
